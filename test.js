7ec34aaf520db42dd473254bdf6d1e091d09be95
Shashank Amin <shashankamin@live.in>
2024-07-19 09:02:19 +0530

    fix: JS_STANDRAD linting issues

--- test.js
+++ test.js
@@ -4,32 +4,32 @@ const qrcode = require('qrcode-terminal');
 const wwebVersion = '2.2412.50';
 
 const client = new Client({
-    authStrategy: new LocalAuth(), // your authstrategy here
-    puppeteer: {
-        headless: true,
-        args: ['--no-sandbox']
-    },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
-    },
+  authStrategy: new LocalAuth(), // your authstrategy here
+  puppeteer: {
+    headless: true,
+    args: ['--no-sandbox'],
+  },
+  webVersionCache: {
+    type: 'remote',
+    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+  },
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
+  qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+  console.log('Client is ready!');
 });
 
 client.on('message', msg => {
-    console.log(msg);
-    if (msg.body == '!ping') {
-        let chat = msg.getChat();
-        chat.sendSeen();
-        msg.reply('pong');
-    }
+  console.log(msg);
+  if (msg.body === '!ping') {
+    const chat = msg.getChat();
+    chat.sendSeen();
+    msg.reply('pong');
+  }
 });
 
 client.initialize();
